eed4d22a74bfac345417b86c64140994e35919c9
hongwai1920 <63549604+hongwai1920@users.noreply.github.com>
2020-06-03 10:13:06 +0800

    Add files via upload

--- scripts/GBM.py
+++ scripts/GBM.py
@@ -1,6 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+class PayOff:
+    def __init__(self, TheOptionsType_, Strike_):
+        '''
+        Inputs:
+        =========
+        TheOptionsType_: string (European call, European put, Binary call, Binary put)
+        Strike_: float strike price
+        '''
+        
+        self.__TheOptionsType = TheOptionsType_
+        self.__Strike = Strike_
+        
+    def __call__(self,spot):
+        # Overloading the ( ) operator
+        '''
+        inputs:
+        =========
+        spot: numpy array of spot prices
+        
+        
+        returns:
+        =========
+        payoff value for each option 
+        '''
+        
+        if self.__TheOptionsType == 'European call':
+            return np.maximum(spot - self.__Strike,0)
+        elif self.__TheOptionsType == 'European put':
+            return np.maximum(self.__Strike - spot,0)
+        elif self.__TheOptionsType == 'Binary call':
+            return np.piecewise(spot, [spot < self.__Strike, spot >= self.__Strike], [0, 1])
+        elif self.__TheOptionsType == 'Binary put':
+            return np.piecewise(spot, [spot <= self.__Strike, spot > self.__Strike], [1, 0])
+        else:
+            raise Exception('Unknown option type found.')
+
 
 def GBM_fd(S0, K, r, d, sigma, T, num_steps = 50, num_paths = 1, plot = False, seed = None):
     '''
